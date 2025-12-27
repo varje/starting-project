aea5bd6be142904d6ce548b58933f67d97543a99
varje <varjekivila@gmail.com>
2025-12-27 11:40:10 +0200

    add name array to post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,10 @@
+const names = ['Varje', 'Virve'];
+
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
   return (
     <div>
-      <p>Varje</p>
+      <p>{chosenName}</p>
       <p>React.js is awesome!</p>
     </div>
   );
