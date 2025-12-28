f5e6e2e9899cc524be7841cc61e24a67255b1437
varje <varjekivila@gmail.com>
2025-12-28 16:29:22 +0200

    add  header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+import MainHeader from './components/MainHeader';
 import PostsList from './components/PostsList';
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
   return (
-    <main>
-      <PostsList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList isPosting={modalIsVisible} onStopPosting={hideModalHandler}/>
+      </main>
+    </>
   );
 }
 
