b8d3c5e24342e3e29e96c4e6eed13e94c7452308
varje <varjekivila@gmail.com>
2025-12-28 16:51:38 +0200

    add form buttons

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,7 +4,7 @@ import Post from './Post';
 import classes from './PostsList.module.css';
 import Modal from './Modal';
 
-function PostsList({isPosting, onStopPosting}) {
+function PostsList({ isPosting, onStopPosting }) {
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
 
@@ -22,6 +22,7 @@ function PostsList({isPosting, onStopPosting}) {
           <NewPost
             onBodyChange={bodyChangeHandler}
             onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
           />
         </Modal>
       )}
