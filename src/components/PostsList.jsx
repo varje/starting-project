91e9ebfb2155602d02b1cd6e00e8a1f606722461
varje <varjekivila@gmail.com>
2025-12-28 16:20:01 +0200

    conditionally show modal

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import classes from './PostsList.module.css';
 import Modal from './Modal';
 
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -17,12 +22,14 @@ function PostsList() {
   }
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
 
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
