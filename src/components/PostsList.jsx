f2cc2337d60d0fe863878f650d93faab4fa6493c
varje <varjekivila@gmail.com>
2026-01-04 12:06:36 +0200

    fetch posts from backend

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,12 +2,25 @@ import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
 import Modal from './Modal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch('http://localhost:8080/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: { 'Content-Type': 'application/json' },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
