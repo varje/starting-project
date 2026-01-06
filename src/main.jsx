81c95ac6013aea56710b90a27b5fed6ac9ca2a9f
varje <varjekivila@gmail.com>
2026-01-06 16:29:17 +0200

    add posts details pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import NewPost, {action as newPostAction} from './routes/NewPost';
+import NewPost, { action as newPostAction } from './routes/NewPost';
 import RootLayout from './routes/RootLayout';
-import Posts, {loader as postsLoader} from './routes/Posts';
+import Posts, { loader as postsLoader } from './routes/Posts';
+import PostDetails, { loader as postDetailsLoader } from './routes/PostDetails';
 
 const router = createBrowserRouter([
   {
@@ -15,7 +16,10 @@ const router = createBrowserRouter([
         path: '/',
         element: <Posts />,
         loader: postsLoader,
-        children: [{ path: '/create-post', element: <NewPost />, action: newPostAction }]
+        children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:id', element: <PostDetails />, loader: postDetailsLoader },
+        ],
       },
     ],
   },
